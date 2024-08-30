ad029df1c0d6e2a32da3db565f6bbd31edfd76ec
Idriss Cisse <aliouc140@gmail.com>
2024-08-30 20:32:06 +0200

    Mise en place du systeme d'authentification

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const booksRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -10,6 +11,8 @@ mongoose.connect('mongodb+srv://aliouc140:0EncXZzawAQWXPeG@projet6oc.jwhww.mongo
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -18,5 +21,6 @@ app.use((req, res, next) => {
   });
 
 app.use('/api/books', booksRoutes); 
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const booksCtrl = require('../controllers/books');
 
+
 const router = express.Router();
+router.post('/', auth,  booksCtrl.createBook);
 
+router.get('/', booksCtrl.getAllBooks);
 
-router.get('/',  booksCtrl.getAllBooks);
-router.post('/',  booksCtrl.createBook);
+router.get('/bestrating', booksCtrl.getBestRating);
+
+router.get('/:id', booksCtrl.getOneBook);
 
 module.exports = router; 
